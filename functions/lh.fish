b64eaea38df7aa6d4c93c80197f78f186d0eb8da
root <account-login@users.noreply.github.com>
2021-06-17 18:21:22 +0800

    fix lh.fish

--- functions/lh.fish
+++ functions/lh.fish
@@ -1,4 +1,3 @@
-# same as lh
-function l --wraps=ls --description 'List contents of directory using long format'
+function lh --wraps=ls --description 'List contents of directory using long format'
     ls -lAh $argv
 end
