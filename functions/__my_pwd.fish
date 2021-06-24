9446e1debd2ebbd4b441c783967c2b8bde739cdf
root <account-login@users.noreply.github.com>
2021-06-24 20:20:29 +0800

    tweak prompt 20210624.

--- functions/__my_pwd.fish
+++ functions/__my_pwd.fish
@@ -4,7 +4,8 @@ function __my_pwd
     set -l prefix_len (string length -- $realhome)
     set -l pwd $PWD
     if test $realhome = (string sub -l $prefix_len -- $pwd)
-        set pwd '~'(string sub -s (math 1 + $prefix_len) $pwd)
+        set pwd '~'(string sub -s (math 1 + $prefix_len) -- $pwd)
     end
+    set pwd (string replace -r '^/cygdrive/' '/' -- $pwd)
     echo $pwd
 end
