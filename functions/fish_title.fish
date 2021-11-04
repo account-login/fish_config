fa18449b7ae9d0bfdd2ab7d9849e1d0c9b1cac10
root <account-login@users.noreply.github.com>
2021-11-04 18:36:47 +0800

    fix hostname in title.

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,4 +1,4 @@
 # Defined in /usr/share/fish/functions/fish_title.fish @ line 1
 function fish_title
-    echo $__PTS@(prompt_hostname):(__my_pwd) \($__CMD_COUNTER\)
+    echo $__PTS@$ALTHOSTNAME:(__my_pwd) \($__CMD_COUNTER\)
 end
