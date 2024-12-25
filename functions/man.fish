6ab2e63fd25ac32c96f4cb360838a5ba94415195
root <root@localhost>
2024-12-26 00:29:05 +0800

    alias man=man --nj --nh

---
+++ functions/man.fish
@@ -0,0 +1,16 @@
+function man --wraps='man --nj --nh' --description 'man with sane formatting'
+    # maximum columns
+    set -l c 120
+    if test $c -gt $COLUMNS
+        set c $COLUMNS
+    end
+
+    # fish built-in man pages
+    set -l manpath $MANPATH
+    set -l fish_manpath $__fish_data_dir/man
+    if test -d $fish_manpath
+        set manpath $fish_manpath $manpath
+    end
+
+    COLUMNS=$c MANPATH=$manpath command man --nj --nh $argv
+end
