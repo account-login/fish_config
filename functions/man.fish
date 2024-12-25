4f6cb368e3a61d97a85c69c4473b4c194fb87b59
root <root@localhost>
2024-12-26 00:35:06 +0800

    fix the previous commit.

--- functions/man.fish
+++ functions/man.fish
@@ -6,7 +6,7 @@ function man --wraps='man --nj --nh' --description 'man with sane formatting'
     end
 
     # fish built-in man pages
-    set -l manpath $MANPATH
+    set -l manpath $MANPATH /usr/share/man
     set -l fish_manpath $__fish_data_dir/man
     if test -d $fish_manpath
         set manpath $fish_manpath $manpath
